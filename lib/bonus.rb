be196ea851e45043a8889ba1336e9ff9856d8816
Lluis Guardiola <lluisguardiola.h@gmail.com>
2019-10-01 07:13:44 +0000

    Automatically backed up by Learn

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -21,7 +21,9 @@ def bonus
   }
 
   #code your solution here:
-  def check_if_alive
+  
+  
+  epic_tragedy[:]
   
 
   #Don't touch the following line! The `bonus` method must return our newly modified epic tragedy hash
